2211801d0d5175a1a86625e66140bac5723671dc
Zach Lasky <zacharylasky123@gmail.com>
2019-07-16 18:45:53 -0400

    roughly styled display component

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return (
+    <div className="display-container">
+      <input className="submit" type="submit" value="0"/>
+    </div>
+  )
 };
+
+export default Display;
